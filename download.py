8c2dfa4f8474a60a873b4cdc186a09fed1ac8b1b
Mikhel <mbarajas@gmail.com>
2024-11-04 00:37:05 -0800

    Update download.py

--- download.py
+++ download.py
@@ -1,8 +1,21 @@
+import os
 import requests
 
 def download_file(url, filename):
     response = requests.get(url)
     if response.status_code == 200:
+        # Comprobar si el archivo ya existe
+        if os.path.exists(filename):
+            # Leer el contenido del archivo existente
+            with open(filename, 'rb') as f:
+                existing_content = f.read()
+            
+            # Comparar con el nuevo contenido
+            if existing_content == response.content:
+                print(f"El archivo '{filename}' ya está actualizado.")
+                return  # No hay cambios, así que no hacemos nada
+        
+        # Guardar el nuevo contenido en el archivo
         with open(filename, 'wb') as f:
             f.write(response.content)
         print(f"Archivo descargado y guardado como {filename}")
@@ -10,6 +23,6 @@ def download_file(url, filename):
         print(f"Error al descargar el archivo: {response.status_code}")
 
 if __name__ == "__main__":
-    url = "https://i.mjh.nz/PlutoTV/mx.xml"  # Reemplaza con la URL del archivo
-    filename = "mx.xml"  # Cambia la extensión según el tipo de archivo
+    url = "https://i.mjh.nz/PlutoTV/ca.xml"  # Reemplaza con la URL del archivo
+    filename = "canada.xml"  # Cambia la extensión según el tipo de archivo
     download_file(url, filename)
