b09a34fa65eb8a0eae2a047417ee162bfe168b49
Mikhel <mbarajas@gmail.com>
2024-11-04 00:43:15 -0800

    Update download.py

--- download.py
+++ download.py
@@ -1,21 +1,8 @@
-import os
 import requests
 
 def download_file(url, filename):
     response = requests.get(url)
     if response.status_code == 200:
-        # Comprobar si el archivo ya existe
-        if os.path.exists(filename):
-            # Leer el contenido del archivo existente
-            with open(filename, 'rb') as f:
-                existing_content = f.read()
-            
-            # Comparar con el nuevo contenido
-            if existing_content == response.content:
-                print(f"El archivo '{filename}' ya está actualizado.")
-                return  # No hay cambios, así que no hacemos nada
-        
-        # Guardar el nuevo contenido en el archivo
         with open(filename, 'wb') as f:
             f.write(response.content)
         print(f"Archivo descargado y guardado como {filename}")
