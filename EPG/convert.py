e94474b1dd6e3343fe8962fbbc916107f52d5565
Mikhel <mbarajas@gmail.com>
2024-11-09 22:11:04 -0800

    Update convert.py

--- EPG/convert.py
+++ EPG/convert.py
@@ -9,7 +9,7 @@ with open('EPG/epg.json', 'r') as json_file:
 xml_data = xmltodict.unparse({"epg": json_data}, pretty=True)
 
 # Guardar el archivo XML
-with open('EPG/epg.xml', 'w') as xml_file:
+with open('epg.xml', 'w') as xml_file:
     xml_file.write(xml_data)
 
 print("Conversión de JSON a XML completada. Archivo guardado como 'epg.xml'.")
