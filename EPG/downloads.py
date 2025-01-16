e341c55e095bc8930a00c227351254a643ea9832
Mikhel <mbarajas@gmail.com>
2025-01-15 20:09:58 -0800

    Update downloads.py

--- EPG/downloads.py
+++ EPG/downloads.py
@@ -25,13 +25,6 @@ def download_file(url, filename):
 if __name__ == "__main__":
     files_to_download = {
         "https://i.mjh.nz/PlutoTV/mx.xml.gz": "EPG/PLUTO/PLUTOmx.xml.gz",  # Reemplaza con la URL y nombre del archivo
-        "https://i.mjh.nz/PlutoTV/us.xml.gz": "EPG/PLUTO/PLUTOus.xml.gz",
-        "https://i.mjh.nz/PlutoTV/ca.xml.gz": "EPG/PLUTO/PLUTOca.xml.gz",
-        "https://i.mjh.nz/SamsungTVPlus/us.xml.gz": "EPG/PLUTO/SAMSus.xml.gz",  # Otro archivo,
-        "https://i.mjh.nz/SamsungTVPlus/ca.xml.gz": "EPG/PLUTO/SAMSca.xml.gz",  # Y otro más
-        "https://i.mjh.nz/Plex/mx.xml.gz": "EPG/PLUTO/PLEXmx.xml.gz",
-        "https://i.mjh.nz/PlutoTV/.channels.json": "EPG/epg.json",
-        "https://epgshare01.online/epgshare01/epg_ripper_DISTROTV1.xml.gz": "EPG/distro.xml.gz",
     }
     
     for url, filename in files_to_download.items():
