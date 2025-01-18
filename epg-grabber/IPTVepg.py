099b33fdb8f4aba6fd415fc51477dd6f4acfa04c
Mikhel <mbarajas@gmail.com>
2025-01-18 02:07:26 -0800

    Update IPTVepg.py

--- epg-grabber/IPTVepg.py
+++ epg-grabber/IPTVepg.py
@@ -60,8 +60,11 @@ def filter_and_build_epg(urls):
         print(f"New EPG saved to {output_file_gz}")
 
 urls = [
-    'https://epgshare01.online/epgshare01/epg_ripper_ALL_SOURCES1.xml.gz',
-      ]
+    'https://epgshare01.online/epgshare01/epg_ripper_MX1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_CL1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
+       ]
 
 if __name__ == "__main__":
     filter_and_build_epg(urls)
