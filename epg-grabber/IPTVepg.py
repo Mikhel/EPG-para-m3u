b6653e0abb67f84b25487847bc7b7d9f9d633fe2
Mikhel <mbarajas@gmail.com>
2025-01-18 02:22:05 -0800

    Update IPTVepg.py

--- epg-grabber/IPTVepg.py
+++ epg-grabber/IPTVepg.py
@@ -61,9 +61,24 @@ def filter_and_build_epg(urls):
 
 urls = [
     'https://epgshare01.online/epgshare01/epg_ripper_MX1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_AR1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_CH1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CL1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_CO1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_CR1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_DK1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_EC1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_PA1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_PE1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_SV1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_UY1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_US_SPORTS1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_DUMMY_CHANNELS.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_DISTROTV1.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_FANDUEL1.xml.gz',
        ]
 
 if __name__ == "__main__":
