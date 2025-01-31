807dc62c8ee5eecbf484446e3ff52cc9d1c427a2
Mikhel <mbarajas@gmail.com>
2025-01-31 04:02:22 -0800

    Update downloadm3u.py

--- M3U/downloadm3u.py
+++ M3U/downloadm3u.py
@@ -24,18 +24,17 @@ def download_file(url, filename):
 
 if __name__ == "__main__":
     files_to_download = {
-      "https://tinyurl.com/multiservice21?region=all&service=SamsungTVPlus": "M3U/SAM/samall",  # Reemplaza con la URL y nombre del archivo
-      "https://tinyurl.com/multiservice21?region=us&service=SamsungTVPlus": "M3U/SAM/samus",
-      "https://tinyurl.com/multiservice21?region=ca&service=SamsungTVPlus": "M3U/SAM/samca",
-      "https://tinyurl.com/multiservice21?region=gb&service=SamsungTVPlus": "M3U/SAM/samgb",
-      "https://tinyurl.com/multiservice21?region=es&service=SamsungTVPlus": "M3U/SAM/sames",
-      "https://tinyurl.com/multiservice21?region=all&service=PlutoTV": "M3U/PLUTO/all",
-      "https://tinyurl.com/multiservice21?region=us&service=PlutoTV": "M3U/PLUTO/us",
-      "https://tinyurl.com/multiservice21?region=ca&service=PlutoTV": "M3U/PLUTO/ca",
-      "https://tinyurl.com/multiservice21?region=gb&service=PlutoTV": "M3U/PLUTO/gb",
-      "https://tinyurl.com/multiservice21?region=mx&service=PlutoTV": "M3U/PLUTO/mx",
-      "https://tinyurl.com/multiservice21?region=es&service=PlutoTV": "M3U/PLUTO/es",
-     
+      "https://tinyurl.com/multiservice21?region=all&service=SamsungTVPlus": "M3U/SAM/samall.m3u",  # Reemplaza con la URL y nombre del archivo
+      "https://tinyurl.com/multiservice21?region=us&service=SamsungTVPlus": "M3U/SAM/samus.m3u",
+      "https://tinyurl.com/multiservice21?region=ca&service=SamsungTVPlus": "M3U/SAM/samca.m3u",
+      "https://tinyurl.com/multiservice21?region=gb&service=SamsungTVPlus": "M3U/SAM/samgb.m3u",
+      "https://tinyurl.com/multiservice21?region=es&service=SamsungTVPlus": "M3U/SAM/sames.m3u",
+      "https://tinyurl.com/multiservice21?region=all&service=PlutoTV": "M3U/PLUTO/all.m3u",
+      "https://tinyurl.com/multiservice21?region=us&service=PlutoTV": "M3U/PLUTO/us.m3u",
+      "https://tinyurl.com/multiservice21?region=ca&service=PlutoTV": "M3U/PLUTO/ca.m3u",
+      "https://tinyurl.com/multiservice21?region=gb&service=PlutoTV": "M3U/PLUTO/gb.m3u",
+      "https://tinyurl.com/multiservice21?region=mx&service=PlutoTV": "M3U/PLUTO/mx.m3u",
+      "https://tinyurl.com/multiservice21?region=es&service=PlutoTV": "M3U/PLUTO/es.m3u",
     }
     
     for url, filename in files_to_download.items():
