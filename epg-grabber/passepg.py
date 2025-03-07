ff1e6edab2b59fcf968f5dc28a9148fca91eb535
Mikhel <mbarajas@gmail.com>
2025-03-07 06:07:55 -0800

    Update passepg.py

--- epg-grabber/passepg.py
+++ epg-grabber/passepg.py
@@ -5,7 +5,7 @@ import requests
 
 save_as_gz = True  # Set to True to save an additional .gz version
 
-tvg_ids_file = os.path.join(os.path.dirname(__file__), 'pass-ids.txt')
+tvg_ids_file = os.path.join(os.path.dirname(__file__), 'passids.txt')
 output_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'EPG/tvpassepg.xml')
 output_file_gz = output_file + '.gz'
 
@@ -61,7 +61,7 @@ def filter_and_build_epg(urls):
 
 urls = [
     'https://github.com/Mikhel/EPG-para-m3u/edit/main/EPG/pass.xml.gz',
-    'https://github.com/Mikhel/EPG-para-m3u/edit/main/EPG/pass2.xml',
+    'https://github.com/Mikhel/EPG-para-m3u/edit/main/EPG/pass2.xml.gz',
        ]
 
 if __name__ == "__main__":
